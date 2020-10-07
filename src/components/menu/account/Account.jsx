0dc59ab403d979e2fc276c9211f150c423724750
Brayan Ñauñay <alexanderbrayan2001_@outlook.com>
2020-10-07 14:31:52 -0500

    cambiando 6

--- src/components/menu/account/Account.jsx
+++ src/components/menu/account/Account.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Menu from './Menu';
-import '../../../assets/styles/menu/account/Account.css';
+import React from "react";
+import Menu from "./Menu";
+import "../../../assets/styles/menu/account/Account.css";
 
 class Account extends React.Component {
   constructor(props) {
@@ -20,41 +20,35 @@ class Account extends React.Component {
     this.setState({
       active: false,
     });
-    console.log('sI ESTAQ FUFNCJKXKS');
+    console.log("sI ESTAQ FUFNCJKXKS");
   };
 
   render() {
-    let clase = 'top-account-options';
-    if (this.state.active) {
-      clase += ' top-account-unfold';
+    const { active } = this.state;
+    let clase = "top-account-options";
+    if (active) {
+      clase += " top-account-unfold";
     }
     return (
-      <div className='top-menu-account' id='account' onMouseEnter={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
-        <Menu
-          className={clase}
-
-          up='top-menu-up'
-        />
-
-        <div
-          className='top-account-profile'
-
-        >
-          <div className='top-account-icon'>
+      <div className='top-menu-account' id='account'>
+        <div className='top-account-profile'>
+          <div
+            className='top-account-icon'
+            onMouseEnter={this.handleMouseOver}
+            onMouseLeave={this.handleMouseLeave}>
             <svg
               width='1.4em'
               height='1.4em'
               viewBox='0 0 16 16'
               className='bi bi-person-fill account-person-icon'
               fill='currentColor'
-              xmlns='http://www.w3.org/2000/svg'
-            >
+              xmlns='http://www.w3.org/2000/svg'>
               <path
                 fillRule='evenodd'
                 d='M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z'
               />
             </svg>
-            <span>Hola, Brayan</span>
+            <Menu className={clase} up='top-menu-up' />
           </div>
         </div>
       </div>
