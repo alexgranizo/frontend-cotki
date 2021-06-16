4753f96c837c3f2df088b1e8e7606a36638f31ba
Brayan Ñauñay <alexanderbrayan2001_@outlook.com>
2021-06-15 19:01:11 -0500

    Corrigiendo errores de estilo en el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,46 +11,49 @@ class Footer extends React.Component {
   render() {
     return (
       <div className='footer'>
-        <div className='footer-container'>
-          <div className='footer-logo-cotki'>
-            <img src={logo} alt='logo cotki' />
-          </div>
-          <div className='footer-description'>
-            Somos una empresa lider en la distribucion de productos de consumo
-            masivo, llegando a donde nadie mas quizo llegar.
-          </div>
-          <hr />
-          <div className='footer-container-network'>
-            <h4>Nuestras redes sociales</h4>
-            <div className='footer-network'>
-              <div className='network-item'>
-                <Facebook height='30px' />
-              </div>
-              <div className='network-item'>
-                <Whatsapp height='30px' />
-              </div>
-              <div className='network-item'>
-                <Youtube height='30px' />
-              </div>
-              <div className='network-item'>
-                <Instagram height='33px' />
+        <div className='footer-capture'>
+          <div className='footer-container'>
+            <div className='footer-logo-cotki'>
+              <img src={logo} alt='logo cotki' />
+            </div>
+            <div className='footer-description'>
+              Somos una empresa lider en la distribucion de productos de consumo
+              masivo, llegando a donde nadie mas quizo llegar.
+            </div>
+            <hr />
+            <div className='footer-container-network'>
+              <h4>Nuestras redes sociales</h4>
+              <div className='footer-network'>
+                <div className='network-item'>
+                  <Facebook height='30px' />
+                </div>
+                <div className='network-item'>
+                  <Whatsapp height='30px' />
+                </div>
+                <div className='network-item'>
+                  <Youtube height='30px' />
+                </div>
+                <div className='network-item'>
+                  <Instagram height='33px' />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <Links className='footer-info' title='Conócenos' name1='Vende tus productos' link1='/' name2='Afiliate' link2='/' name3='Trabaja con nosotros' link3='/' name4='Agenda una cita' link4='/' />
-        <Links className='footer-others' title='Gana dinero con nosotros' name1='¿Cómo ser socio?' link1='/' name2='Developers' link2='/' name3='Vacantes' link3='/' name4='Referidos' link4='/' />
-
-        <div className='footer-links'>
-          <hr width='100%' className='footer-lines' />
-          <a href='/'>Política de uso</a>
-          <a href='/'>Aviso legal</a>
-          <a href='/'>Política de privacidad</a>
-          <a href='/'>Política de reembolso</a>
-          <a href='/'>Política de envíos</a>
-          <a href='/'>Política de geolocalización</a>
-          <a href='/'>Base legal</a>
-          <a href='/'>Aviso</a>
+          <Links className='footer-info' title='Conócenos' name1='Vende tus productos' link1='/' name2='Afiliate' link2='/' name3='Trabaja con nosotros' link3='/' name4='Agenda una cita' link4='/' />
+          <Links className='footer-others' title='Gana dinero con nosotros' name1='¿Cómo ser socio?' link1='/' name2='Developers' link2='/' name3='Vacantes' link3='/' name4='Referidos' link4='/' />
+          <div className='footer-links'>
+            <hr width='100%' className='footer-lines' />
+            <div className='footer-links-after'>
+              <a href='/'>Política de uso</a>
+              <a href='/'>Aviso legal</a>
+              <a href='/'>Política de privacidad</a>
+              <a href='/'>Política de reembolso</a>
+              <a href='/'>Política de envíos</a>
+              <a href='/'>Política de geolocalización</a>
+              <a href='/'>Base legal</a>
+              <a href='/'>Aviso</a>
+            </div>
+          </div>
         </div>
       </div>
     );
